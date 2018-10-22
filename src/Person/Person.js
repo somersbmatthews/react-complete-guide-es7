f6e996b83c953b0913477e457223d15ae3665d60
Somers Matthews <somersbmatthews@gmail.com>
2018-10-22 15:53:57 -0500

    added Switch Name button to App.js

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,8 +4,8 @@ const person = (props) => {
     return (
         <div>
     
-            <p>I'm {props.name} and I am {props.age} years old!</p>
-            <p>{props.children}</p>
+             <p>I'm {props.name} and I am {props.age} years old!</p>
+             <p>{props.children}</p>
 
         </div>
     
