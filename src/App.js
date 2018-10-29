e78a1a06b0f2efd2d98e46b5fc800126ad17e1c9
Somers Matthews <somersbmatthews@gmail.com>
2018-10-29 14:26:45 -0500

    added assignedClasses variable to make up for classes variable mismatch from new class import in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import './App.css';
-
 import Person from './Person/Person';
+
+import classes from './App.css';
+
+
 
 
 class App extends Component {
@@ -88,19 +90,19 @@ class App extends Component {
       };
     }
 
-    const classes = []
+    const assignedClasses = []
     if (this.state.persons.length <= 2) {
-      classes.push('red'); 
+      assignedClasses.push(classes.red); 
     }
     if (this.state.persons.length <= 1) {
-      classes.push('bold');
+      assignedClasses.push(classes.bold);
     }
 
     return (
       
-        <div className="App">
+        <div className={classes.App}>
           <h1>Hi, I'm a React App.</h1>
-          <p className={classes.join(' ')}>This is really working!</p>
+          <p className={assignedClasses.join(' ')}>This is really working!</p>
           <button 
             style={style}
             onClick={this.togglePersonsHandler}>Toggle Persons</button>
