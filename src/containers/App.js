5eafe26724010477eafd93e4415820460c20ca0d
Somers Matthews <somersbmatthews@gmail.com>
2019-01-24 23:21:07 -0600

    removed Error Boundary

--- src/containers/App.js
+++ src/containers/App.js
@@ -75,14 +75,14 @@ class App extends Component {
       persons = (
         <div>  
           {this.state.persons.map((person, index) => {
-            return <ErrorBoundary key={person.id}>
+             
           <Person 
               click={this.deletePersonHandler.bind(index)}
               name={person.name}
               age={person.age} 
-              
+              key={person.id}
               changed={(event) => this.nameChangedHandler(event, person.id)}/>
-            </ErrorBoundary>
+              
           })}
          </div> 
       )
